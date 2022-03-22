ea2ea2a1aff3ad2aaba9f6c22a91a326774ea2e7
344300zhd <2929616615@qq.com>
2022-03-22 12:40:57 +0800

    Delete src directory

--- src/api/FileApi.js
+++
@@ -1,9 +0,0 @@
-import http from "@/utils/http.js"
-
-export function uploadFile(params){
-    return http.upload("/api/uploadFile",params)
-}
-
-export function downloadFile(params){
-    return http.download(`/api/downloadFile/${params}`)
-}
